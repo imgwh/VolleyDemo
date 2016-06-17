cee6cfbaa433030da839b3e18012fe7315ac50df
imgwh <365326932@qq.com>
2016-06-17 17:02:52 +0800

    ImageRequest的用法

--- app/src/main/java/cn/nanguage/volleydemo/MainActivity.java
+++ app/src/main/java/cn/nanguage/volleydemo/MainActivity.java
@@ -1,13 +1,16 @@
 package cn.nanguage.volleydemo;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -17,14 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = (ImageView)this.findViewById(R.id.imageView);
         //1、创建请求队列
-        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        /**2、实例化StringRequest请求
+        /*RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        *//**2、实例化StringRequest请求
          *  第一个参数选择Request.Method.GET即get请求方式
          *  第二个参数的url地址根据文档所给
          *  第三个参数Response.Listener 请求成功的回调
          *  第四个参数Response.ErrorListener 请求失败的回调
-         */
+         *//*
         StringRequest stringRequest = new StringRequest(Request.Method.GET,"http://marsweather.ingenology.com/v1/latest/",
                 new Response.Listener<String>() {
                     @Override
@@ -39,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //3、将请求添加进请求队列
-        requestQueue.add(stringRequest);
+        requestQueue.add(stringRequest);*/
+
+        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
+        ImageRequest imageRequest = new ImageRequest(
+                "http://i3.3conline.com/images/piclib/201008/06/batch/1/66354/1281085968511jlnbuklwca_medium.jpg",
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imageView.setImageBitmap(response);
+                    }
+                }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        mQueue.add(imageRequest);
     }
 }
